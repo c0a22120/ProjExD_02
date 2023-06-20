229db3cfb3a3b961b1181cd5dd8db5d6d0800e1a
Kuma <c0a2212031@edu.teu.ac.jp>
2023-06-20 14:38:49 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,13 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
+delta = {
+    pg.K_UP:(0, -5),
+    pg.K_DOWN:(0,+5),
+    pg.K_LEFT:(-5, 0),
+    pg.K_RIGHT:(+5, 0),
+}
+
 
 
 def main():
@@ -12,6 +19,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bom_img = pg.Surface((20,20))
     pg.draw.circle(bom_img,(255,0,0),(10,10),10)
     bom_img.set_colorkey((0,0,0))
@@ -19,7 +28,7 @@ def main():
     y = random.randint(0, HEIGHT)
     bom_rct = bom_img.get_rect()    #抽出
     bom_rct.center =x,y     #爆弾Rectの中心座標を乱数で指定する
-    vx, vy = +10, -10
+    vx, vy = +5, -5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -27,8 +36,17 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
+        key_lst = pg.key.get_pressed() #keyを押すやつ
+        sum_mv = [0,0]
+        for k, mv in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+            kk_rct.move_ip(sum_mv)
+
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         bom_rct.move_ip(vx,vy)
         screen.blit(bom_img, bom_rct)
         pg.display.update()
